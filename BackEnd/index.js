a29370bbc118165acfd16d7fa35ee2d18a0423c2
Faisal <faisalcse29@gmail.com>
2018-10-04 16:15:28 +0600

    mongodb connection established

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,4 +3,5 @@ const app = express();
 const port = 3001;
 const { router } = require('./Router');
 app.use('/v1', router);
+require('./configDatabase');
 app.listen(port, () => console.log(`app is listening to ${port}`))
--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="col-6" style={{backgroundColor:'red'}}>
+      <div className="col-6" >
         <h1>Hello there</h1>
       </div>
     );
