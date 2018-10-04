08af21a7753bd4c5995d1931588ef5f1d0518443
Faisal <faisalcse29@gmail.com>
2018-10-04 14:26:30 +0600

    router initiated in the project
    now every route will be randled with router

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-app.get('/', (req, res) => res.send('Hello World!'));
+const { router } = require('./Router');
+app.use('/v1', router);
 app.listen(port, () => console.log(`app is listening to ${port}`))
